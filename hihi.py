9bb619847be4f8b1a56478acff512decad38e1fc
toanloi2569 <toanloi2569@gmail.com>
2019-05-24 20:42:20 +0700

    .

--- hihi.py
+++ hihi.py
@@ -1,74 +1,105 @@
-from __future__ import print_function
 import sys
 import cv2
 from random import randint
+import argparse
+import numpy as np
 
-if __name__ == '__main__':
-    # đường dẫn đến  videop
-    videoPath = "videos/3.mp4"
-    # tạo đối tượng chứa nội dung video đọc từ file lên
-    cap = cv2.VideoCapture(videoPath)
-    # đọc frame đầu tiên của video
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to vieos need detection")
+ap.add_argument("-s", "--scaleFacter", help="float")
+ap.add_argument("-m", "--minNeighbors", help="int")
+ap.add_argument("-m2", "--minSize", help="tuple. ex (30, 30)")
+ap.add_argument("-m3", "--maxSize", help="tuple. ex (60, 60)")
+args = vars(ap.parse_args())
+
+if args["video"] is None:
+    videoPath = 0
+else: videoPath = args["video"]
+if args["scaleFacter"] is None:
+    scaleFacter = 1.08
+else: scaleFacter = float(args["scaleFacter"])
+if args["minNeighbors"] is None:
+    minNeighbors = 8
+else: minNeighbors = int(args["minNeighbors"])
+# if args["minSize"] is None:
+minSize = (30, 30)
+# else: minSize = args["minSize"]
+# if args["maxSize"] is None:
+maxSize = (40, 40)
+# else: maxSize = args["maxSize"]
+
+haar_cascade_face = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+multiTracker = cv2.MultiTracker_create()
+cap = cv2.VideoCapture("videos/1.mp4")
+colors = []
+
+def read_frame():
     success, frame = cap.read()
-    # n = 99
-    # for i in range(n):
-    #     success, frame = cap.read()
-    # kiểm tra xem có thành công không , nếu không thì thoát ra
+
     if not success:
         print('Failed to read video')
         sys.exit(1)
+    return frame
 
-    ## chuyển ảnh thành màu xám
+def detect_face(frame):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ## tải trình phân loại khuôn mặt đã được đào tạo trong opencv
-    haar_cascade_face = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    ## lấy các hình chữ nhật có khuôn mặt được phát hiện , đầu vào của hàm này là một ảnh xám
-    bboxes = haar_cascade_face.detectMultiScale(frame_gray, scaleFactor=1.08, minNeighbors=5 , minSize=(30,30))
-    ## tạo một mảng để chứa màu cho các đối tượng
-    colors = []
+    bboxes = haar_cascade_face.detectMultiScale(frame_gray, scaleFactor=scaleFacter, minNeighbors=minNeighbors , minSize=minSize, maxSize=maxSize)
+    return bboxes
 
-    # Tạo đối tượng để chứa các tracker
-    multiTracker = cv2.MultiTracker_create()
+def add_tracker(frame, box):
+    colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
+    tracker = cv2.TrackerCSRT_create()
+    multiTracker.add(tracker, frame, tuple(box))
+    return frame
 
-    # Khởi tạo các tracker cho từng đối tượng mà trình phát hiện đã phát hiện ra
-    for bbox_ in bboxes:
-        # vì đầu vào của hàm dưới là tuple nên phải chuyển ndarray thành tuple
-        bbox = (bbox_[0],bbox_[1],bbox_[2],bbox_[3])
-        colors.append((randint(64, 255), randint(64, 255), randint(64, 255)))
-        tracker = cv2.TrackerCSRT_create()
-        multiTracker.add(tracker, frame, bbox)
+def check_new_face(box1, boxs):
+    midpoint = [box1[0] + box1[2]*0.5 , box1[1] + box1[3]*0.5]
+    for box2 in boxs:
+        if midpoint[0] > box2[0] and midpoint[0] < box2[0] + box2[2]:
+            if midpoint[1] > box2[1] and midpoint[1] < box2[1] + box2[3]:
+                return False
+    return True
 
-    # for i, newbox in enumerate(bboxes):
-    #     p1 = (int(newbox[0]), int(newbox[1]))
-    #     p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
-    #     cv2.rectangle(frame, p1, p2, colors[i], 2, 1)
-    # from matplotlib import pyplot as plt
-    # plt.imshow(frame)
-    # plt.show()
+def draw(frame, box, ncolor):
+    p1 = (int(box[0]), int(box[1]))
+    p2 = (int(box[0] + box[2]), int(box[1] + box[3]))
+    cv2.rectangle(frame, p1, p2, colors[ncolor], 2, 1)
+    return frame
 
-    while True :
-        # lấy một khung mới để tracking
-        success, frame = cap.read()
-        if not success:
-            print("Không lấy được thêm khung mới")
-            break
+bboxes = None
+while True:
+    frame = read_frame()
+    bboxes = detect_face(frame)
+    if len(bboxes) > 0:
+        break
+for ncolor, box in enumerate(bboxes):
+    add_tracker(frame, box)
+    draw(frame, box, ncolor)
 
-        # cập nhật vị trí của đối tượng với một khung mới
-        success, boxes = multiTracker.update(frame)
-        if not success:
-            print("Không tìm thấy đối tượng trong khung mới")
-            #continue
-        # vẽ hình bao cho đối tượng
-        for i, newbox in enumerate(boxes):
-            p1 = (int(newbox[0]), int(newbox[1]))
-            p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
-            cv2.rectangle(frame, p1, p2, colors[i], 2, 1)
+count = 1
+boxes = []
+while True :
+    count += 1
+    frame = read_frame()
+    success, boxes = multiTracker.update(frame)
+    if not success:
+        print("Không tìm thấy đối tượng trong khung mới")
 
-        # show frame
-        cv2.imshow('MultiTracker', frame)
+    if count % 10 == 0:
+        newboxs = detect_face(frame)
+        print (newboxs)
+        for i in newboxs:
+            if check_new_face(i, boxes):     
+                add_tracker(frame, i)
+                boxes = np.append(boxes, [i], axis=0)
 
-        # Nhấn dấu cách để kết thúc
-        k = cv2.waitKey(50) & 0xFF
-        if k == 32:
-            break
+    for ncolor, box in enumerate(boxes):
+        draw(frame, box, ncolor)
 
+    # show frame
+    cv2.imshow('MultiTracker', frame)
+
+    # Nhấn dấu cách để kết thúc
+    k = cv2.waitKey(50) & 0xFF
+    if k == 32:
+        break
